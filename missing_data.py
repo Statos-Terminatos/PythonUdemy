31fff20f63101a3058baf6c9b7384cd499898d6b
Aliya Ussinova <aussinova@gmail.com>
2017-09-15 15:38:19 +0200

    Taking care of missing data

--- missing_data.py
+++ missing_data.py
@@ -5,7 +5,7 @@ import pandas as pd
 dataset = pd.read_csv('./Machine Learning A-Z/Part 1 - Data Preprocessing/Data.csv')
 
 X = dataset.iloc[:,:-1].values
-Y = dataset.iloc[:, 3].values
+y = dataset.iloc[:, 3].values
 
 from sklearn.preprocessing import Imputer
 # strategy correspond the method
@@ -15,4 +15,10 @@ imputer = Imputer(missing_values = "NaN", axis=0, strategy="mean")
 imputer = imputer.fit(X[:,1:3]) # the upper bond is not included, index starts at zero
 X[:,1:3] = imputer.transform(X[:,1:3])
 
-print(X)
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:,0] = labelencoder_X.fit_transform(X[:,0])
+onehotencoder = OneHotEncoder(categorical_features=[0])
+X = onehotencoder.fit_transform(X).toarray()
+labelencoder_y = LabelEncoder()
+Y = labelencoder_y.fit_transform(y)
